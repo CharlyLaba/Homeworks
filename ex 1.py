c0d186a016b610dbdcf657a24bc41c521e569525
CharlyLaba <charly.chedrawi@hotmail.com>
2018-12-10 10:32:19 +0100

    Initial commit

---
+++ ex 1.py
@@ -0,0 +1,9 @@
+name = input("What is your name?")
+
+#print("Hello "+name+"!")
+#What is the difference between doing things with a function and without a function?
+
+def say_hi(name):
+    print("Hello "+name+"!")
+
+say_hi(name)
